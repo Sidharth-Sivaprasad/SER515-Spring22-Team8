73584da08af21c370b612aadc576f9d658f26e93
Sidharth Koyipallil Sivaprasad (Student) <sksivapr@sundevils.asu.edu>
2022-04-07 01:46:17 -0700

    Rover adding text correection

--- Rover_Maze_integrated/Assets/MazeGenerator/Scripts/MazeSpawner.cs
+++ Rover_Maze_integrated/Assets/MazeGenerator/Scripts/MazeSpawner.cs
@@ -24,7 +24,7 @@ public class MazeSpawner : MonoBehaviour {
 	public float CellWidth = 5;
 	public float CellHeight = 5;
 	public bool AddGaps = true;
-	public GameObject GoalPrefab = null;
+
 
 	private BasicMazeGenerator mMazeGenerator = null;
 
@@ -72,10 +72,6 @@ public class MazeSpawner : MonoBehaviour {
 				}
 				if(cell.WallBack){
 					tmp = Instantiate(Wall,new Vector3(x,0,z-CellHeight/2)+Wall.transform.position,Quaternion.Euler(0,180,0)) as GameObject;// back
-					tmp.transform.parent = transform;
-				}
-				if(cell.IsGoal && GoalPrefab != null){
-					tmp = Instantiate(GoalPrefab,new Vector3(x,1,z), Quaternion.Euler(0,0,0)) as GameObject;
 					tmp.transform.parent = transform;
 				}
 			}
